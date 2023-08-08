416c441f31b7bcb58ac11de3b58048d18c41d44c
AshakaDinith <gunarath-cs18014@stu.kln.ac.lk>
2023-08-08 19:31:38 +0530

    create marrnet APP

--- src/MANETAPP.java
+++ src/MANETAPP.java
@@ -1,17 +1,152 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.io.*;
+import java.net.*;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+
+public class MANETAPP {
+    private String username;
+    private int port;
+    private ServerSocket serverSocket;
+    private SecretKeySpec secretKey;
+
+
+    public MANETAPP(String username, int port, String passphrase) {
+        this.username = username;
+        this.port = port;
+        this.secretKey = generateSecretKey(passphrase);
+    }
+
+
+    private SecretKeySpec generateSecretKey(String passphrase) {
+        try {
+            MessageDigest sha = MessageDigest.getInstance("SHA-256");
+            byte[] keyBytes = sha.digest(passphrase.getBytes(StandardCharsets.UTF_8));
+            keyBytes = truncateKey(keyBytes, 16); // Use only the first 16 bytes for 128-bit AES key
+            return new SecretKeySpec(keyBytes, "AES");
+        } catch (Exception e) {
+            throw new RuntimeException("Error generating secret key.", e);
+        }
+    }
+
+    private byte[] truncateKey(byte[] key, int length) {
+        byte[] truncatedKey = new byte[length];
+        System.arraycopy(key, 0, truncatedKey, 0, length);
+        return truncatedKey;
+    }
+
+
+    private String encryptMessage(String message) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] encryptedBytes = cipher.doFinal(message.getBytes());
+            return Base64.getEncoder().encodeToString(encryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error encrypting message.", e);
+        }
+    }
+
+    private String decryptMessage(String encryptedMessage) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] encryptedBytes = Base64.getDecoder().decode(encryptedMessage);
+            byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+            return new String(decryptedBytes);
+        } catch (Exception e) {
+            throw new RuntimeException("Error decrypting message.", e);
+        }
+    }
+
+
+    public void start() {
+        try {
+            serverSocket = new ServerSocket(port);
+            new Thread(() -> acceptIncomingMessages()).start();
+        } catch (IOException e) {
+            throw new RuntimeException("Error starting the server socket.", e);
+        }
+    }
+
+
+    private void acceptIncomingMessages() {
+        try {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Thread(() -> handleIncomingMessage(socket)).start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error accepting incoming connections.", e);
+        }
+    }
+
+
+    private void handleIncomingMessage(Socket socket) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            String message;
+            while ((message = reader.readLine()) != null) {
+                String decryptedMessage = decryptMessage(message);
+                if (decryptedMessage != null) {
+                    System.out.println(decryptedMessage);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error handling incoming message.", e);
+        }
+    }
+
+
+    public void sendMessage(List<Integer> destPorts, String message) {
+        String encryptedMessage = encryptMessage(username + ": " + message);
+        if (encryptedMessage != null) {
+            for (Integer port : destPorts) {
+                try (Socket socket = new Socket("localhost", port);
+                     PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
+                    writer.println(encryptedMessage);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.print("Enter your username: ");
+            String username = bufferedReader.readLine();
+            System.out.print("Enter the port number for this node: ");
+            int port = Integer.parseInt(bufferedReader.readLine());
+            System.out.print("Enter the passphrase: ");
+            String passphrase = bufferedReader.readLine();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            MANETAPP node = new MANETAPP(username, port, passphrase);
+            node.start();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            List<Integer> destPorts = new ArrayList<>();
+            while (true) {
+                System.out.print("> ");
+                String message = bufferedReader.readLine();
+                if (message.equalsIgnoreCase("exit")) {
+                    break;
+                }
+                System.out.print("Enter destination port numbers (comma-separated): ");
+                String[] portNumbers = bufferedReader.readLine().split(",");
+                destPorts.clear();
+                for (String portStr : portNumbers) {
+                    destPorts.add(Integer.parseInt(portStr.trim()));
+                }
+                node.sendMessage(destPorts, message);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading input: " + e.getMessage());
         }
     }
 }
